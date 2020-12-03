615a2f020c60c3b3fc0c653114a6b0985f06ee12
JP <joao.virgili@carupi.com>
2020-12-02 21:58:19 -0300

    Ensure HttpAdapter returns data if get returns 200

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -3,19 +3,22 @@ import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
+import 'package:movies/data/http/http.dart';
 
 class DioMock extends Mock implements Dio {}
 
-class HttpAdapter {
+class HttpAdapter implements HttpClient {
   final Dio dio;
 
   HttpAdapter(this.dio);
 
-  Future<void> get({
+  @override
+  Future<Map> get({
     @required String url,
     Map<String, dynamic> queryParameters,
   }) async {
-    return dio.get(url, queryParameters: queryParameters);
+    final dioResponse = await dio.get(url, queryParameters: queryParameters);
+    return dioResponse.data as Map;
   }
 }
 
@@ -25,6 +28,7 @@ void main() {
   String url;
 
   final queryParametersMock = {'apiKey': ''};
+  final responseMock = {'a': 'a'};
 
   setUp(() {
     client = DioMock();
@@ -32,16 +36,31 @@ void main() {
     url = faker.internet.httpUrl();
   });
 
+  PostExpectation mockRequest({Map<String, dynamic> query}) =>
+      when(client.get(any, queryParameters: query));
+
+  Future<Response<dynamic>> mockResponse(Invocation _) async =>
+      Response(data: responseMock);
+
   group('get', () {
     test('Should call get with correct values', () async {
+      mockRequest(query: queryParametersMock).thenAnswer(mockResponse);
       await sut.get(url: url, queryParameters: queryParametersMock);
 
       verify(client.get(url, queryParameters: queryParametersMock));
     });
     test('Should call get without queryParameters', () async {
+      mockRequest().thenAnswer(mockResponse);
       await sut.get(url: url);
 
       verify(client.get(url));
+    });
+    test('Should return data if get returns 200', () async {
+      mockRequest().thenAnswer(mockResponse);
+
+      final data = await sut.get(url: url);
+
+      expect(data, {'a': 'a'});
     });
   });
 }
