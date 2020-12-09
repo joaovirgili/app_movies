8e84888265338d4742ac056ed11eecc28181bb64
JP <joao.virgili@carupi.com>
2020-12-08 23:19:03 -0300

    Add constant app colors

---
+++ lib/ui/shared/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const blue01 = Color(0xff00384C);
+
+  static const gray06 = Color(0xff343A40);
+  static const gray05 = Color(0xff6D7070);
+  static const gray04 = Color(0xff5E6770);
+  static const gray03 = Color(0xff868E96);
+  static const gray02 = Color(0xffE9ECEF);
+  static const gray01 = Color(0xffF1F3F5);
+  static const gray00 = Color(0xffF5F5F5);
+
+  static const white = Colors.white;
+}
