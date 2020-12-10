2555e69c1c72fce7684ecac3ea571550ac5fea4d
JP <joao.virgili@carupi.com>
2020-12-10 00:28:16 -0300

    Add Equatable to GenreUiModel

--- lib/ui/modules/home/models/genre_ui_model.dart
+++ lib/ui/modules/home/models/genre_ui_model.dart
@@ -1,8 +1,12 @@
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
-class GenreUiModel {
+class GenreUiModel extends Equatable {
   final String id;
   final String name;
 
   GenreUiModel({@required this.id, @required this.name});
+
+  @override
+  List<Object> get props => [id];
 }
