8811cca854cdcff3921b7cbbf84132b74b6c0810
JP <joao.virgili@carupi.com>
2020-12-10 22:39:46 -0300

    Fixed api paths

--- lib/shared/api/api.dart
+++ lib/shared/api/api.dart
@@ -1,10 +1,10 @@
 class Api {
-  static const baseUrl = 'https://api.themoviedb.org/3/';
-  static const movie = 'movie/';
-  static const genreList = 'genre/movie/list/';
-  static const discover = 'discover/movie/';
+  static const baseUrl = 'https://api.themoviedb.org/3';
+  static const movie = '/movie';
+  static const genreList = '/genre/movie/list';
+  static const discover = '/discover/movie';
 
   static const defaultLanguage = 'pt-br';
 
-  static const imageBaseUrl = 'https://image.tmdb.org/t/p/w500/';
+  static const imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
 }
