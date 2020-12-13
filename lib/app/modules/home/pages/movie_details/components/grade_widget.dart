32c5ee54109196a4df2a92a7a84ef9224056e3f5
JP <joao.virgili@carupi.com>
2020-12-12 21:03:10 -0300

    Implement GradeWidget for movie details

---
+++ lib/app/modules/home/pages/movie_details/components/grade_widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../shared/styles.dart';
+
+class GradeWidget extends StatelessWidget {
+  const GradeWidget({
+    @required this.grade,
+    Key key,
+  }) : super(key: key);
+
+  final double grade;
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        text: '$grade',
+        style: AppStyles.movieGradeTextStyle,
+        children: [
+          TextSpan(
+            text: '  /  ${10}',
+            style: AppStyles.maxGradeTextStyle,
+          ),
+        ],
+      ),
+    );
+  }
+}
