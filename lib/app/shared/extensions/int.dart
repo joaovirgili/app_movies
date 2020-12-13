72d5ec92169fc633123a5b6618cf2409f6107fb7
JP <sr.joaovirgili@gmail.com>
2020-12-13 03:21:37 -0300

    Add extension por currency

--- lib/app/shared/extensions/int.dart
+++ lib/app/shared/extensions/int.dart
@@ -4,4 +4,10 @@ extension formatRuntime on int {
     List<String> parts = d.toString().split(':');
     return '${parts[0].padLeft(2, '0')}h ${parts[1].padLeft(2, '0')}min';
   }
+
+  String toSimpleCurrency() {
+    return '\$ ' +
+        this.toString().replaceAllMapped(
+            RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},');
+  }
 }
